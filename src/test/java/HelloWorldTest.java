06262bfa1ff4367a850608e29ab52048343313f1
Jay <jayakrishnantu.007@gmail.com>
2023-10-30 22:52:58 -0700

    surefire plugin

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+public class HelloWorldTest {
+
+    public void testHello(){
+        System.out.println("testing hello world!");
+    }
+
+    public void testWorld(){
+        System.out.println("testing again hello world!");
+    }
+}
